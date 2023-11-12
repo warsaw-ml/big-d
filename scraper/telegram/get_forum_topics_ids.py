cc29b3c977d74c4be69dcd9f79bc2530c60926e9
Lukas <lukasz.zalewski.ai@gmail.com>
2023-11-12 16:41:01 +0100

    Update pubsub (#1)

--- scraper/telegram/get_forum_topics_ids.py
+++ scraper/telegram/get_forum_topics_ids.py
@@ -1,23 +1,19 @@
-import asyncio
 import os
 from datetime import datetime
 
 import autorootcwd
+import yaml
 from rich import print
 from telethon import events, functions
 from telethon.sync import TelegramClient
 
-forums = {
-    "floppingblue": 1849376414,
-    "ape_aperton": 183422978,
-    "gainz": 1854179726,
-}
+# load forums from yaml
+with open("data/tg_forums.yaml") as f:
+    forums = yaml.load(f, Loader=yaml.FullLoader)
 
 
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
-
-
 client = TelegramClient("anon", api_id, api_hash).start()
 
 
@@ -36,28 +32,15 @@ def get_topics():
     for topic in forum_topics.topics:
         print(topic.title, topic.id)
         ids.append(topic.from_id)
-        # ids.append(topic.id)
 
-    # print(ids)
     print("Number of rooms:", len(ids))
-    print("Listening for messages...")
 
     return ids
 
 
 ids = get_topics()
 
-
-@client.on(events.NewMessage(chats=ids, incoming=True))
-async def handler(event):
-    print("NEW MESSAGE!")
-    print(event.raw_text)
-
-    # download media like .webm
-    # if event.media:
-    #     media = await client.download_media(event.media)
-    #     print(media)
-    #     print(type(media))
-
-
-client.run_until_disconnected()
+# save ids to file
+with open("data/tg_forum_ids.txt", "w") as f:
+    for id in ids:
+        f.write(str(id) + "\n")
