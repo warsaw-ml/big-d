7b3ab07d19c0d32ac664bed6816e3c100e6277fc
wojtas000 <jan2000@op.pl>
2023-11-15 01:42:46 +0100

    update dataflow pipeline

---
+++ dataflow/run_dataflow.py
@@ -0,0 +1,23 @@
+import subprocess
+import os
+
+WINDOW_SIZE = 2
+RUNNER = 'DataflowRunner'
+PROJECT_ID = 'big-d-project-404815'
+REGION = 'europe-central2'
+BUCKET_NAME = 'big-d-project-master-dataset'
+
+command = [
+    'python', 
+    './full_dataflow.py',
+    f'--project={PROJECT_ID}',
+    f'--region={REGION}',
+    f'--window_size={WINDOW_SIZE}',
+    f'--runner={RUNNER}',
+    f'--temp_location=gs://{BUCKET_NAME}/temp'
+]
+
+try:
+    subprocess.run(command, check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
