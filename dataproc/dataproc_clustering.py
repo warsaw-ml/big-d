cfb79b3d5023b58607e9e96548af56c63f7407ff
wojtas000 <jan2000@op.pl>
2023-12-18 22:49:03 +0100

    fix dataproc clustering, remove redundant clustering file

--- dataproc/dataproc_clustering.py
+++ dataproc/dataproc_clustering.py
@@ -11,13 +11,12 @@ import logging
 EMBEDDING_SIZE = 768
 
 
-# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Create a Spark session
 spark = SparkSession.builder.appName("EmbeddingsClustering").getOrCreate()
-logger.info("Spark session created")
+logger.info("1. Spark session created")
 
 # Read Parquet files from GCS and filter those with 'embedding' column
 parquet_path = "gs://big-d-project-master-dataset/parquet/*.parquet"
@@ -32,9 +31,13 @@ df = df_list[0]
 for temp_df in df_list[1:]:
     df = df.unionByName(temp_df, allowMissingColumns=True)
 
+logger.info("2. Parquet files read from GCS")
+
 # drop duplicates, filter records with null embedding and filter records with embedding size != 768
 df = df.dropDuplicates()
 df = df.filter(col("embedding").isNotNull()).filter(size("embedding") == EMBEDDING_SIZE)
+
+logger.info("3. Duplicates and null values in embeddings dropped")
 
 # select only the message_id and embedding column
 list_to_vector_udf = udf(lambda l: Vectors.dense(l), VectorUDT())
@@ -43,11 +46,15 @@ embedding = df.select(
     list_to_vector_udf(df["embedding"]).alias("embedding")
     )
 
+logger.info("4. Embedding column selected")
+
 # Create a KMeans model
 k = 5
-kmeans = KMeans(k=k, seed=1, featuresCol="embedding", predictionCol="cluster").setMaxIter(5)
+kmeans = KMeans(k=k, seed=1, featuresCol="embedding", predictionCol="cluster")
 model = kmeans.fit(embedding)
 predictions = model.transform(embedding)
+
+logger.info("5. KMeans model created and predictions made")
 
 # Save message_id and cluster columns to GCS
 csv_path = "gs://big-d-project-master-dataset/csv/embeddings_clustering.csv"
